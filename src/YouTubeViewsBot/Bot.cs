9c6af089fb311de6011ba80ae6a093afacd7f5f1
Gabriel Isaac <iamkemical1@gmail.com>
2021-03-01 02:30:21 +0100

    Added few changes to the bot

--- src/YouTubeViewsBot/Bot.cs
+++ src/YouTubeViewsBot/Bot.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium.Firefox;
 using System;
+using System.Threading;
 
 namespace YouTubeViewsBot
 {
@@ -7,7 +8,11 @@ namespace YouTubeViewsBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Initializes the Firefox driver
+            Startup();
+
+            // Initializes the youtube video
+            ViewVideo();
         }
 
         public static FirefoxDriver fireFoxDriver;
@@ -16,7 +21,7 @@ namespace YouTubeViewsBot
             FirefoxOptions options = new FirefoxOptions();
 
             // Makes browser to hide
-            options.AddArgument("--headless");
+            //options.AddArgument("--headless");
             FirefoxDriverService firefoxDriverService = FirefoxDriverService.CreateDefaultService();
             firefoxDriverService.HideCommandPromptWindow = true;
             fireFoxDriver = new FirefoxDriver(firefoxDriverService, options);
@@ -24,7 +29,23 @@ namespace YouTubeViewsBot
 
         static void ViewVideo()
         {
-            // 
+            // Go the link of the video
+            string url = "https://www.youtube.com/watch?v=oEILJYhx1RQ";
+            fireFoxDriver.Navigate().GoToUrl(url);
+
+
+            // No of views of the video
+            int noOfViews = 1000;
+
+            // Iterates the number of times the video is viewed
+            for (int i = 1; i <= noOfViews; i++)
+            {
+                Thread.Sleep(10000);
+                fireFoxDriver.Navigate().Refresh();
+                
+                ConsoleLog.ConsoleLogger($"Video viewed {i} times");
+            }
+            fireFoxDriver.Quit();
         }
     }
 
