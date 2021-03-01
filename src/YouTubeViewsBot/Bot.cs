c0ea081b5bdd608728dcfe43882a57c7661935b5
Gabriel Isaac <iamkemical1@gmail.com>
2021-03-01 01:21:51 +0100

    Added project and installed selenium driver

---
+++ src/YouTubeViewsBot/Bot.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+
+namespace YouTubeViewsBot
+{
+    class Bot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static FirefoxDriver fireFoxDriver;
+        static void Startup()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+
+            // Makes browser to hide
+            options.AddArgument("--headless");
+            FirefoxDriverService firefoxDriverService = FirefoxDriverService.CreateDefaultService();
+            firefoxDriverService.HideCommandPromptWindow = true;
+            fireFoxDriver = new FirefoxDriver(firefoxDriverService, options);
+        }
+
+        static void ViewVideo()
+        {
+            // 
+        }
+    }
+
+    
+}
