99d566f4cf143856e1ee69fb476d909c84cddca0
Gabriel Isaac <iamkemical1@gmail.com>
2021-03-01 01:24:14 +0100

    Added ConsoleLogger.

---
+++ src/YouTubeViewsBot/ConsoleLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeViewsBot
+{
+    public class ConsoleLog
+    {
+        public static void ConsoleLogger(string s)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {s}");
+        }
+    }
+}
